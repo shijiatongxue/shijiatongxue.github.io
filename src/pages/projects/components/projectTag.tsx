e4924c73b437d48efe0925d7523cc6126c4551b8
石嘉 <shijia.me@bytedance.com>
2023-05-28 00:24:35 +0800

    feat: add projects page

--- src/pages/projects/components/projectTag.tsx
+++ src/pages/projects/components/projectTag.tsx
@@ -2,16 +2,18 @@ const tagColorMap = {
   React: "#38bdf8ff",
   Figma: "#F43F5E",
   D2C: "#111827ff",
-  'Design to code': "#111827ff",
-  'UI components': "#bada55",
+  "Design to code": "#111827ff",
+  "UI components": "#bada55",
 };
 
 export default function ProjectTag(props: { name: string }) {
   const { name } = props;
-  const bg = `bg-[${tagColorMap[name] ?? "#38bdf8ff"}]`;
   return (
     <div
-      className={`shrink-0 flex justify-center items-center gap-x-[8px] py-[8px] px-[16px] rounded-[32px] ${bg}`}
+      className="shrink-0 flex justify-center items-center gap-x-[8px] py-[8px] px-[16px] rounded-[32px]"
+      style={{
+        background: tagColorMap[name],
+      }}
     >
       <p className="shrink-0 font-['Poppins'] text-[16px] text-[#ffffffff] leading-[16px] text-left align-top font-[400]">
         {name}
