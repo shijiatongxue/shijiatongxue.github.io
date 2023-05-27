ccaad6a3a0101b4325c405dac6b15f850791b804
石嘉 <shijia.me@bytedance.com>
2023-05-28 00:18:09 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { splitVendorChunkPlugin } from "vite";
 
-const isProd = process.env.NODE_ENV === 'production';
-const cdn = 'https://site-1307850796.cos.ap-beijing.myqcloud.com';
+const isProd = process.env.NODE_ENV === "production";
+const cdn = "https://site-1307850796.cos.ap-beijing.myqcloud.com";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isProd ? cdn : '/',
+  base: isProd ? cdn : "/",
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
-  plugins: [react()],
-})
+  plugins: [splitVendorChunkPlugin(), react()],
+});
