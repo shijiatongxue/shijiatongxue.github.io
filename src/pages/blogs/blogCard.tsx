5840f136044902316f5d0f29f6a5b5db23c1990c
石嘉 <shijia.me@bytedance.com>
2023-06-25 23:12:32 +0800

    docs: update blogs

--- src/pages/blogs/blogCard.tsx
+++ src/pages/blogs/blogCard.tsx
@@ -12,11 +12,11 @@ const BlogCard = (props: Blog) => {
       onClick={handleGoBlog}
       className="box-border w-full py-[12px] px-[12px] sm:py-[24px] sm:px-[30px] sm:h-[260px] flex items-center justify-between gap-[20px] border-b border-solid border-[var(--semi-color-border)] rounded-[20px] bg-[var(--semi-bg-0)] cursor-pointer"
     >
-      <div className="flex shrink flex-col items-start gap-y-[4px] sm:gap-y-[30px] grow overflow-hidden">
+      <div className="flex shrink flex-col items-start gap-y-[4px] sm:gap-y-[12px] grow overflow-hidden">
         <p className="shrink min-w-[107px] text-[13px] text-[--semi-color-text-1] leading-[16px] align-top font-[500]">
           {createTime.toLocaleDateString()}
         </p>
-        <div className="shrink items-start gap-y-[10px] overflow-hidden w-full">
+        <div className="flex flex-col shrink items-start gap-y-[8px] overflow-hidden w-full">
           <p className="box-border truncate text-[26px] text-[--semi-color-text-0] leading-[34px] text-left align-top font-[600]">
             {title}
           </p>
@@ -26,9 +26,8 @@ const BlogCard = (props: Blog) => {
         </div>
       </div>
       <img
-        className="box-border w-[300px] h-[212px] rounded-[10px] hidden	sm:inline-block"
-        width="300"
-        height="212"
+        className="box-border w-[350px] rounded-[10px] hidden	sm:inline-block"
+        width="350"
         src={coverUrl}
       />
     </div>
