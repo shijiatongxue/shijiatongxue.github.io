7e386a8a2d3e64591ab72ee377f6e2ce43137cd5
shijia.me <shijia.me@bytedance.com>
2023-07-30 18:54:54 +0800

    feat: update blogs

--- src/pages/blogs/blogCard.tsx
+++ src/pages/blogs/blogCard.tsx
@@ -1,7 +1,7 @@
 import { Blog } from "./useBlogList";
 
 const BlogCard = (props: Blog) => {
-  const { createTime, coverUrl, title, url, description } = props;
+  const { createTime, coverUrl, title, url, description, updateTime } = props;
 
   const handleGoBlog = () => {
     window.open(url);
@@ -13,8 +13,13 @@ const BlogCard = (props: Blog) => {
       className="box-border w-full py-[12px] px-[12px] sm:py-[24px] sm:px-[30px] sm:h-[260px] flex items-center justify-between gap-[20px] border-b border-solid border-[var(--semi-color-border)] rounded-[20px] bg-[var(--semi-bg-0)] cursor-pointer"
     >
       <div className="flex shrink flex-col items-start gap-y-[4px] sm:gap-y-[12px] grow overflow-hidden">
-        <p className="shrink min-w-[107px] text-[13px] text-[--semi-color-text-1] leading-[16px] align-top font-[500]">
-          {createTime.toLocaleDateString()}
+        <p className="flex shrink gap-x-2 min-w-[107px] text-[13px] text-[--semi-color-text-1] leading-[16px] align-top font-[500]">
+          {`${createTime.toLocaleDateString()}`}
+          {updateTime !== createTime && (
+            <div title={`编辑于 ${updateTime.toLocaleDateString()}`} className="text-[--semi-color-text-3]">
+              已编辑
+            </div>
+          )}
         </p>
         <div className="flex flex-col shrink items-start gap-y-[8px] overflow-hidden w-full">
           <p className="box-border truncate text-[26px] text-[--semi-color-text-0] leading-[34px] text-left align-top font-[600]">
--- src/pages/blogs/useBlogList.ts
+++ src/pages/blogs/useBlogList.ts
@@ -8,6 +8,7 @@ export interface Blog {
   title: string;
   description: string;
   createTime: Date;
+  updateTime: Date;
   coverUrl?: string;
 }
 
@@ -15,7 +16,7 @@ export interface Blog {
  * @url https://www.notion.so/Blog-8b5996a9eeed460facf2233b8dffe386
  */
 function formatData(rawData: Record<string, any>): Blog {
-  const { id, cover, created_time, url, properties, paragraph } = rawData;
+  const { id, cover, created_time, url, properties, paragraph, last_edited_time } = rawData;
   const siteUrl = url.replace(
     "https://www.notion.so",
     "https://shijia.notion.site"
@@ -28,6 +29,7 @@ function formatData(rawData: Record<string, any>): Blog {
     coverUrl: externalUrl || fileUrl,
     description: get(paragraph, "paragraph.rich_text[0].plain_text"),
     createTime: new Date(created_time),
+    updateTime: new Date(last_edited_time),
     url: siteUrl,
   };
   return result;
